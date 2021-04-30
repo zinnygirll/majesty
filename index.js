31bc31aff5250fbd88ec8f13a714ff9c67122677
Ezinne <ezinneadindu63@gmail.com>
2021-04-30 22:13:46 +0100

    Adding Updates

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const SERVER_PORT = 4000;
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname+'/public/home.html'))
+})
+
+//app.listen(SERVER_PORT, () => console.log('Server listening on port ' + SERVER_PORT));
+
+app.listen(process.env.PORT || SERVER_PORT, () =>
+    console.log("Server listening on port " + SERVER_PORT)
+);
